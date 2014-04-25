fbe9b886158257edbfefc2fc5ca6e1aca64c34fe
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-04-25 21:07:40 +0200

    Add vcr and webmock for testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
