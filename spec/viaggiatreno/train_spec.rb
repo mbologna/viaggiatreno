dbacf3d79d351629634ef85c5e3044cd8005c51f
Michele Bologna <michele.bologna@gmail.com>
2016-01-12 16:45:49 +0100

    Small adjustments for Rubocop

--- spec/viaggiatreno/train_spec.rb
+++ spec/viaggiatreno/train_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'viaggia_treno'
 
 describe Train do
-
   before do
     VCR.use_cassette('Train new') do
       @train = Train.new('20241')
@@ -10,9 +9,11 @@ describe Train do
   end
 
   describe '#to_s' do
-    it { expect(@train.to_s).to eq \
-      "20241 REG 20241: Il treno e' arrivato con 2 minuti di ritardo state: ARRIVED,     delay: 2, lastUpdate:  "
-    }
+    it do
+      expect(@train.to_s).to eq \
+        '20241 REG 20241: Il treno e\' arrivato con 2 minuti di ritardo state'\
+        ': ARRIVED,     delay: 2, lastUpdate:  '
+    end
   end
 
   describe '#trainStops' do
@@ -22,29 +23,31 @@ describe Train do
       end
     end
 
-    it { expect(@train_stops.map(&:to_s)).to eq [
-      '[X] DOMODOSSOLA = SCHEDULED: 18:10 EXPECTED: 18:11',
-      '[X] VOGOGNA OSSOLA = SCHEDULED: 18:18 EXPECTED: 18:20',
-      '[X] PREMOSELLO CHIOVENDA = SCHEDULED: 18:21 EXPECTED: 18:22',
-      '[X] CUZZAGO = SCHEDULED: 18:25 EXPECTED: 18:27',
-      '[X] MERGOZZO = SCHEDULED: 18:31 EXPECTED: 18:32',
-      '[X] VERBANIA-PALLANZA = SCHEDULED: 18:35 EXPECTED: 18:36',
-      '[X] BAVENO = SCHEDULED: 18:40 EXPECTED: 18:42',
-      '[X] STRESA = SCHEDULED: 18:44 EXPECTED: 18:46',
-      '[X] BELGIRATE = SCHEDULED: 18:49 EXPECTED: 18:53',
-      '[X] LESA = SCHEDULED: 18:53 EXPECTED: 18:57',
-      '[X] MEINA = SCHEDULED: 18:57 EXPECTED: 19:00',
-      '[X] ARONA = SCHEDULED: 19:02 EXPECTED: 19:06',
-      '[X] DORMELLETTO = SCHEDULED: 19:07 EXPECTED: 19:11',
-      '[X] SESTO CALENDE = SCHEDULED: 19:12 EXPECTED: 19:16',
-      '[X] VERGIATE = SCHEDULED: 19:17 EXPECTED: 19:21',
-      '[X] SOMMA LOMBARDO = SCHEDULED: 19:21 EXPECTED: 19:25',
-      '[X] CASORATE SEMPIONE = SCHEDULED: 19:25 EXPECTED: 19:30',
-      '[X] GALLARATE = SCHEDULED: 19:32 EXPECTED: 19:33',
-      '[X] BUSTO ARSIZIO = SCHEDULED: 19:38 EXPECTED: 19:39',
-      '[X] RHO = SCHEDULED: 20:00 EXPECTED: 19:57',
-      '[X] MILANO PORTA GARIBALDI = SCHEDULED: 20:13 EXPECTED: 20:15']
-    }
+    it do
+      expect(@train_stops.map(&:to_s)).to eq [
+        '[X] DOMODOSSOLA = SCHEDULED: 18:10 EXPECTED: 18:11',
+        '[X] VOGOGNA OSSOLA = SCHEDULED: 18:18 EXPECTED: 18:20',
+        '[X] PREMOSELLO CHIOVENDA = SCHEDULED: 18:21 EXPECTED: 18:22',
+        '[X] CUZZAGO = SCHEDULED: 18:25 EXPECTED: 18:27',
+        '[X] MERGOZZO = SCHEDULED: 18:31 EXPECTED: 18:32',
+        '[X] VERBANIA-PALLANZA = SCHEDULED: 18:35 EXPECTED: 18:36',
+        '[X] BAVENO = SCHEDULED: 18:40 EXPECTED: 18:42',
+        '[X] STRESA = SCHEDULED: 18:44 EXPECTED: 18:46',
+        '[X] BELGIRATE = SCHEDULED: 18:49 EXPECTED: 18:53',
+        '[X] LESA = SCHEDULED: 18:53 EXPECTED: 18:57',
+        '[X] MEINA = SCHEDULED: 18:57 EXPECTED: 19:00',
+        '[X] ARONA = SCHEDULED: 19:02 EXPECTED: 19:06',
+        '[X] DORMELLETTO = SCHEDULED: 19:07 EXPECTED: 19:11',
+        '[X] SESTO CALENDE = SCHEDULED: 19:12 EXPECTED: 19:16',
+        '[X] VERGIATE = SCHEDULED: 19:17 EXPECTED: 19:21',
+        '[X] SOMMA LOMBARDO = SCHEDULED: 19:21 EXPECTED: 19:25',
+        '[X] CASORATE SEMPIONE = SCHEDULED: 19:25 EXPECTED: 19:30',
+        '[X] GALLARATE = SCHEDULED: 19:32 EXPECTED: 19:33',
+        '[X] BUSTO ARSIZIO = SCHEDULED: 19:38 EXPECTED: 19:39',
+        '[X] RHO = SCHEDULED: 20:00 EXPECTED: 19:57',
+        '[X] MILANO PORTA GARIBALDI = SCHEDULED: 20:13 EXPECTED: 20:15'
+      ]
+    end
   end
 
   describe '#departingStation' do
