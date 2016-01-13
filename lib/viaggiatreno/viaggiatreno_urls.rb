0c7f85e25126623ad7f2a1bdf26adaa9256fd603
Michele Bologna <michele.bologna@gmail.com>
2016-01-13 09:17:33 +0100

    Style fixing

--- lib/viaggiatreno/viaggiatreno_urls.rb
+++ lib/viaggiatreno/viaggiatreno_urls.rb
@@ -1,12 +1,14 @@
 require_relative 'regex_match_info'
 
 class ViaggiatrenoURLs
+  SITE_INFO = 'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/'
   SITE_INFO_MAIN =
-      'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/numero?numeroTreno=' + \
-      RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE + \
-      '&tipoRicerca=numero&lang=IT'
+    SITE_INFO + 'numero?numeroTreno=' +
+    RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE + \
+    '&tipoRicerca=numero&lang=IT'
   SITE_INFO_DETAILS =
-      'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/scheda?dettaglio=visualizza&numeroTreno=' + \
-      RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE + \
-      '&tipoRicerca=numero&lang=IT'
+    SITE_INFO +
+    'scheda?dettaglio=visualizza&numeroTreno=' + \
+    RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE + \
+    '&tipoRicerca=numero&lang=IT'
 end
--- spec/viaggiatreno_spec.rb
+++ spec/viaggiatreno_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'viaggiatreno'
 
 describe Viaggiatreno do
-
   describe '#version' do
     subject { Viaggiatreno::VERSION }
     it { should be_a String }
