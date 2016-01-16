2b28206e1b9eed3292e6bf64e9a1464b1de9d297
Michele Bologna <michele.bologna@gmail.com>
2016-01-16 14:59:15 +0100

    Using #tr instead of #gsub

--- lib/viaggiatreno/string_utils.rb
+++ lib/viaggiatreno/string_utils.rb
@@ -1,6 +1,6 @@
 class StringUtils
   # utility method
   def self.remove_newlines_tabs_and_spaces(str)
-    str.content.delete("\r").delete("\n").gsub(/\t/, ' ').gsub(/ +/, ' ').strip
+    str.content.delete("\r").delete("\n").tr("\t", ' ').gsub(/ +/, ' ').strip
   end
 end
