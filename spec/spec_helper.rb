86fae561e02d7747620e4405632030f11339fe39
Michele Bologna <michele.bologna@gmail.com>
2016-01-16 15:37:46 +0100

    Specify VCR to send results to CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,10 @@ require 'vcr'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr_cassettes'
-  c.hook_into :webmock
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
 
 RSpec.configure do |config|
