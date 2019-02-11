817bcbf00b57763c515697a678f804b0b33ba5ca
Michele Bologna <michele.bologna@gmail.com>
2019-02-11 21:21:17 +0100

    Refactor: freeze const regex

--- lib/viaggiatreno/regex_match_info.rb
+++ lib/viaggiatreno/regex_match_info.rb
@@ -1,13 +1,14 @@
 class RegExpMatchInfo
   # regex to match train status (string)
-  REGEXP_STATE_TRAVELING = /(Il treno viaggia.*)(Ultimo rilevamento a)(.*)/
-  REGEXP_STATE_NOT_DEPARTED = /Il treno non e' ancora partito/
-  REGEXP_STATE_ARRIVED = /Il treno e' arrivato.*/
-  REGEXP_DELAY_STR = /con (\d+) minuti di ([anticipo|ritardo]+)/
-  REGEXP_NODELAY_STR = /Il treno .* in orario.*/
-  REGEXP_STOP_ALREADY_DONE = /giaeffettuate/
-  REGEXP_SCHEDULED_RAIL = /Binario Previsto: (\d+|--)/
-  REGEXP_ACTUAL_RAIL = /Binario Reale: (\d+|--)/
+  REGEXP_STATE_TRAVELING =
+    /(Il treno viaggia.*)(Ultimo rilevamento a)(.*)/.freeze
+  REGEXP_STATE_NOT_DEPARTED = /Il treno non e' ancora partito/.freeze
+  REGEXP_STATE_ARRIVED = /Il treno e' arrivato.*/.freeze
+  REGEXP_DELAY_STR = /con (\d+) minuti di ([anticipo|ritardo]+)/.freeze
+  REGEXP_NODELAY_STR = /Il treno .* in orario.*/.freeze
+  REGEXP_STOP_ALREADY_DONE = /giaeffettuate/.freeze
+  REGEXP_SCHEDULED_RAIL = /Binario Previsto: (\d+|--)/.freeze
+  REGEXP_ACTUAL_RAIL = /Binario Reale: (\d+|--)/.freeze
   STR_DELAY_STR = 'ritardo'.freeze
   STR_TRAIN_NUMBER_URL_REPLACE = 'TRAINNUMBER'.freeze
 end
