e5c83269ff728146c246fc5c29e27bf8ef208cfc
Michele Bologna <michele.bologna@gmail.com>
2019-02-11 21:20:10 +0100

    Chore: remove deprecated codeclimate gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,8 @@
 require 'vcr'
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
-  config.ignore_hosts 'codeclimate.com'
 end
 
 RSpec.configure do |config|
