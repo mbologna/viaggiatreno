c6597f388cb1e5995c3005d2c34331fa7f433c29
Michele Bologna <michele.bologna@gmail.com>
2019-02-11 21:28:34 +0100

    Style: split long lines

--- lib/viaggiatreno/xpath_match_info.rb
+++ lib/viaggiatreno/xpath_match_info.rb
@@ -3,7 +3,8 @@ class XPathMatchInfo
   XPATH_STATUS = '//div[@class="evidenziato"]/strong'.freeze
   XPATH_TRAIN_NAME = '//h1/text()'.freeze
   XPATH_TRAIN_GENERIC_INFO = '//div[@class="corpocentrale"]'.freeze
-  XPATH_DETAILS_GENERIC = '//div[@class="giaeffettuate"] | //div[@class="corpocentrale"]'.freeze
+  XPATH_DETAILS_GENERIC =
+    '//div[@class="giaeffettuate"] | //div[@class="corpocentrale"]'.freeze
   XPATH_DETAILS_STATION_NAME = 'h2/text()'.freeze
   XPATH_DETAILS_SCHEDULED_STOP_TIME = 'p[1]/strong[1]/text()'.freeze
   XPATH_DETAILS_ACTUAL_STOP_TIME = 'p[2]/strong[1]/text()'.freeze
