09fcf73871385429adcc105280430a56afd6932f
Michele Bologna <michele.bologna@gmail.com>
2019-02-14 21:01:08 +0100

    Refactor: simplify URL builder

--- lib/viaggiatreno/utils/scrape/viaggiatreno_urls.rb
+++ lib/viaggiatreno/utils/scrape/viaggiatreno_urls.rb
@@ -2,14 +2,18 @@ require_relative '../regex/regex_match_info'
 
 class ViaggiatrenoURLs
   VIAGGIATRENO_URL = 'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/'.freeze
-  SITE_TRAIN_INFO_STATUS =
-    VIAGGIATRENO_URL + 'numero?numeroTreno=' +
-    RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE +
+  SITE_STATION_INFO = (VIAGGIATRENO_URL + 'stazione').freeze
+
+  def self.getTrainInfoURL(train_number)
+    return VIAGGIATRENO_URL + 'numero?numeroTreno=' +
+    train_number +
     '&tipoRicerca=numero&lang=IT'
-  SITE_TRAIN_INFO_DETAILS =
-    SITE_TRAIN_INFO_STATUS +
+  end
+
+  def self.getTrainInfoDetailsURL(train_number)
+    return VIAGGIATRENO_URL +
     'scheda?dettaglio=visualizza&numeroTreno=' +
-    RegExpMatchInfo::STR_TRAIN_NUMBER_URL_REPLACE +
+    train_number +
     '&tipoRicerca=numero&lang=IT'
-  SITE_STATION_INFO = VIAGGIATRENO_URL + 'stazione'
+  end
 end
