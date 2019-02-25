617904bf3340adb25669b5608e77799386a18c9f
Michele Bologna <michele.bologna@gmail.com>
2019-02-25 12:32:20 +0100

    Style: use conditional expressions

--- lib/viaggiatreno/model/train/train.rb
+++ lib/viaggiatreno/model/train/train.rb
@@ -32,9 +32,7 @@ class Train
   def arriving_station
     if state != TrainState::TRAVELING
       stops.reverse_each do |stop|
-        if stop.state == TrainStopState::DONE
-          return stop.train_station
-        end
+        return stop.train_station if stop.state == TrainStopState::DONE
       end
     end
     stops.last.train_station
@@ -83,9 +81,8 @@ class Train
   end
 
   def actual_arriving_platform
-    if state == TrainState::NOT_DEPARTED
-      return nil
-    end
+    return nil if state == TrainState::NOT_DEPARTED
+
     stops.last.actual_platform
   end
 
@@ -94,9 +91,8 @@ class Train
   end
 
   def last_stop
-    if state == TrainState::NOT_DEPARTED or state == TrainState::SUPPRESSED
-      return nil
-    end
+    return nil if state == TrainState::NOT_DEPARTED || state == TrainState::SUPPRESSED
+
     stops.each_with_index do |stop, index|
       if index != stops.size - 1 and stop.state == TrainStopState::DONE &&
          stops[index + 1].state != TrainStopState::DONE
@@ -108,9 +104,7 @@ class Train
 
   def find_stop(station_name)
     stops.each do |stop|
-      if stop.train_station.casecmp(station_name).zero?
-        return stop
-      end
+      return stop if stop.train_station.casecmp(station_name).zero?
     end
     raise StopNotFound.new
   end
